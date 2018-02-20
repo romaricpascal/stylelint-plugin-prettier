1ed604ec584e04e9cce2eced37709bbc5b8bc4eb
Romaric Pascal <romaric.pascal@gmail.com>
2018-02-20 07:04:53 +0000

    style: Main file linting

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 // Abbreviated asynchronous example
-var stylelint = require("stylelint")
+var stylelint = require("stylelint");
 
-var ruleName = "prettier/prettier"
-var messages =  stylelint.utils.ruleMessages(ruleName, {
-  expected: "Expected ...",
-})
+var ruleName = "prettier/prettier";
+var messages = stylelint.utils.ruleMessages(ruleName, {
+  expected: "Expected ..."
+});
 
-module.exports = stylelint.createPlugin(ruleName, function(primaryOption, secondaryOptionObject) {
-  return function(postcssRoot, postcssResult) {
-    var validOptions = stylelint.utils.validateOptions(postcssResult, ruleName)
-    if (!validOptions) { return }
+module.exports = stylelint.createPlugin(ruleName, function(
+  primaryOption,
+  secondaryOptionObject
+) {});
 
-    return new Promise(function(resolve) {
-      // some async operation
-      setTimeout(function() {
-        // ... some logic ...
-        stylelint.utils.report({ .. })
-        resolve()
-      }, 1)
-    })
-  }
-})
-
-module.exports.ruleName = ruleName
-module.exports.messages = messages
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
