8599a5673dd23b58cd3778af7f5615ba020fb6c1
Romaric Pascal <romaric.pascal@gmail.com>
2018-02-22 07:45:10 +0000

    feat: Added prettier check as a basic first implementation
    Showed that stylelint-test-rule-tape introduces default tests... that fail Prettier formatting so
    need to be removed :(

--- index.js
+++ index.js
@@ -5,13 +5,32 @@ var ruleName = "prettier/prettier";
 var messages = stylelint.utils.ruleMessages(ruleName, {
   expected: "Expected ..."
 });
-
+// Prettier is pretty heavy to load, so lazy loading it
+// when first rule gets executed
+var prettier;
 module.exports = stylelint.createPlugin(
   ruleName,
   function() /* primaryOption,
   secondaryOptionObject */
   {
-    return function() {};
+    return function(root, result) {
+      if (!prettier) {
+        prettier = require("prettier");
+      }
+
+      var isFormattingOK = prettier.check(root.source.input.css);
+
+      if (!isFormattingOK) {
+        stylelint.utils.report({
+          ruleName: ruleName,
+          result: result,
+          node: root,
+          line: 0,
+          column: 0,
+          message: "Formatting doesn't match Prettier's"
+        });
+      }
+    };
   }
 );
 
