54dacfe1f83c771d0455f917a8002a02830f81df
Romaric Pascal <romaric.pascal@gmail.com>
2018-03-09 08:41:54 +0000

    test: Added logic for testing fixes

--- rules/prettier/__tests__/index.js
+++ rules/prettier/__tests__/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 const FOLDERS = ["fixtures/accept", "fixtures/reject", "fixtures/fixed"];
-const FILES = [["basic.css"], ["basic.css"]];
+const FILES = [["basic.css"], ["basic.css"], ["basic.css"]];
 
 const testRule = require("../../../test/testRule");
 const rule = require("..");
@@ -19,18 +19,31 @@ const contents = FILES.map((fileList, index) =>
   })
 );
 
+let hasFixes;
 const tests = {
   accept: contents[0].map(file => ({
     code: file.contents
   })),
-  reject: contents[1].map(file => ({
-    code: file.contents
-  }))
-};
+  reject: contents[1].map(file => {
+    const rejection = {
+      code: file.contents
+    };
+    const fix = contents[2].findIndex(
+      potentialFix => potentialFix.fileName === file.fileName
+    );
 
+    if (fix !== -1) {
+      hasFixes = true;
+      rejection.fixed = contents[2][fix].contents;
+    }
+
+    return rejection;
+  })
+};
 testRule(
   rule,
   Object.assign({}, tests, {
-    ruleName: rule.ruleName
+    ruleName: rule.ruleName,
+    fix: hasFixes
   })
 );
